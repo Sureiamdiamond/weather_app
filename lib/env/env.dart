3c3e84d9d9f5d98d65d3acc433a7843687a5e1b0
Parsa Banitaba <106591478+Sureiamdiamond@users.noreply.github.com>
2025-03-31 13:33:57 +0330

    Update env.dart

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -5,5 +5,5 @@ import 'package:envied/envied.dart';
 @Envied(path: '.env')
 final class Env {
   @EnviedField(varName: 'KEY1')
-  static const String forecastApiKey ="7ede0f3a77484c84984122018252302";
+  static const String forecastApiKey ="apikey";
 }
